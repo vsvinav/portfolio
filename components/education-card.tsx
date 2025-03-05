f3e5b67661caee1bbf07cc85040dd3439ad93c38
Vinav Sancheti <vsvinavsancheti@gmail.com>
2025-03-05 15:43:03 -0500

    update education card

--- components/education-card.tsx
+++ components/education-card.tsx
@@ -2,11 +2,10 @@ interface EducationCardProps {
   school: string;
   degree: string;
   duration: string;
-  gpa: string;
 }
 
 export function EducationCard(props: EducationCardProps) {
-  const { school, degree, duration, gpa } = props;
+  const { school, degree, duration } = props;
   return (
     <div className="p-6 rounded-lg border flex flex-col gap-1">
       <div className="flex items-center justify-between">
@@ -14,7 +13,6 @@ export function EducationCard(props: EducationCardProps) {
         <div className="font-semibold">{duration}</div>
       </div>
       <div>{degree}</div>
-      <div>{gpa}</div>
     </div>
   );
 }
